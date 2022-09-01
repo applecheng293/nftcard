1d3382ff9505b49006f9137c7f226bcf1a2071d4
applecheng293 <applecheng293@gmail.com>
2022-09-02 07:52:26 +0800

    修改key

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = "xhie8ncs3OrvUiHLGrdegGAMy5vidly6"
+    const api_key = "WDsi5ciMR_cd0axtpD9jf4dQfpRi71GL"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
@@ -39,7 +39,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = "xhie8ncs3OrvUiHLGrdegGAMy5vidly6"
+      const api_key = "WDsi5ciMR_cd0axtpD9jf4dQfpRi71GL"
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
       const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
